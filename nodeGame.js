62380fd2308ce2f06017584247876651869af32f
Coinnich <>
2025-02-14 17:36:36 +1300

    converted the game to typescript

--- nodeGame.js
+++ nodeGame.js
@@ -1,132 +1,122 @@
-const prompt = require('prompt-sync')({sigint: true});
-
-const fieldElemenets = "^O░*".split("")
-
-class Field {
-  constructor(array2d){
-    this.field = array2d;
-    this.currentPosition = {
-      first: 0,
-      second: 0
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prompt_sync_plus_1 = require("prompt-sync-plus");
+var config = {};
+var prompt = (0, prompt_sync_plus_1.default)(undefined);
+var fieldElements = ["^", "O", "░", "*"];
+var Field = /** @class */ (function () {
+    function Field(array2d) {
+        this.field = array2d;
+        this.currentPosition = {
+            first: 0,
+            second: 0
+        };
     }
-    this.acceptableMoves = "ldur".split("")
-  }
-  print(){
-    for( const row of this.field){
-      row.forEach((fieldCharacter, i)=>{
-        const length = row.length
-        process.stdout.write(fieldCharacter)
-        if(length - 1 === i){
-          process.stdout.write("\n");
+    Field.prototype.print = function () {
+        for (var _i = 0, _a = this.field; _i < _a.length; _i++) {
+            var row = _a[_i];
+            process.stdout.write(row.join(""));
         }
-      })
-    }
-		}
-  checkField(position){
-    return fieldElemenets.map(field=>this.field[position.first][position.second]===field)
-  }
-  getNewPosition(first, second){
-    const newPosition = {
-      first:first,
-      second:second
-    }
-    return newPosition
-  }
-  isNewPositionInBoard(position){
-    return this.field.length > position.first && position.first >= 0 && this.field[0].length > position.second && position.second >= 0
-  }
-  checkLegalMove(input){
-    const {first, second} = this.currentPosition
-    let position;
-    switch(input){
-      case 'd':
-        position = this.getNewPosition(first +1, second)
-        break;
-      case 'l':
-        position = this.getNewPosition(first , second - 1 )
-        break;
-      case 'r':
-        position = this.getNewPosition(first , second + 1 )
-        break
-      case 'u':
-        position = this.getNewPosition(first - 1, second )
-        break;
-      default:
-        console.log('that is not a correct move\nCorrect moves are l, d, u or r')
-        return true
-    }
-  
-    // needed in case movement is legal
-    // result is not in array
-    if(!this.isNewPositionInBoard(position)){
-      console.log("Area out of bounds");
-      return true
-    }
-    // destructure array to improve readability
-    const [win, lose] = this.checkField(position)
-    if(win){
-      // you win
-      console.log("You win by reaching the hat!! congratulations")
-      return true
-    } 
-    if(lose){
-      // you fell down a hole
-      console.log("You fell down a whole")
-      return true
-    } 
-    // else a legal move
-    // update the Fields currentPosition
-    this.currentPosition = position
-    return false
-  }
-  moveCharacter(){
-    this.field[this.currentPosition.first][this.currentPosition.second] = fieldElemenets[3]
-  }
-  static generateRandomFieldItem(percentage = 0.1){
-    
-    const randomNumber = Math.random()
-    if( randomNumber >= 0 && randomNumber < percentage){
-      return fieldElemenets[1];
-    } 
-    return fieldElemenets[2]
-    
-  }
-  static placeHatOnField(fieldArray, height, width ){
-    let h = 0
-    let w = 0
-    do {
-      h = Math.floor(Math.random() * height)
-      w = Math.floor(Math.random() * width)
-    } while ( !h || !w)
-
-    fieldArray[h][w] = fieldElemenets[0]
-  }
-  static generateField(height, width, percentage){
-    const actualPercentage = percentage / 100
-    const fieldArray = new Array()
-    for (let i= 0;i< height;i++){
-      const newArray = []
-      for (let j=0;j<width;j++){
-        newArray.push(Field.generateRandomFieldItem(actualPercentage))
-      }
-      fieldArray.push(newArray)
-    }
-    Field.placeHatOnField(fieldArray, height, width)
-    fieldArray[0][0] = fieldElemenets[3]
-    return fieldArray;
-  }
-}
-
-const fieldArray = Field.generateField(4, 5, 33)
-
-const field = new Field(fieldArray)
-let result = false
+    };
+    Field.prototype.checkField = function (position) {
+        var _this = this;
+        return fieldElements.map(function (field) { return _this.field[position.first][position.second] === field; });
+    };
+    Field.prototype.getNewPosition = function (first, second) {
+        var newPosition = {
+            first: first,
+            second: second
+        };
+        return newPosition;
+    };
+    Field.prototype.isNewPositionInBoard = function (position) {
+        return this.field.length > position.first && position.first >= 0 && this.field[0].length > position.second && position.second >= 0;
+    };
+    Field.prototype.checkLegalMove = function (input) {
+        var _a = this.currentPosition, first = _a.first, second = _a.second;
+        var position;
+        switch (input) {
+            case 'd':
+                position = this.getNewPosition(first + 1, second);
+                break;
+            case 'l':
+                position = this.getNewPosition(first, second - 1);
+                break;
+            case 'r':
+                position = this.getNewPosition(first, second + 1);
+                break;
+            case 'u':
+                position = this.getNewPosition(first - 1, second);
+                break;
+            default:
+                console.log('that is not a correct move\nCorrect moves are l, d, u or r');
+                return true;
+        }
+        // needed in case movement is legal
+        // result is not in array
+        if (!this.isNewPositionInBoard(position)) {
+            console.log("Area out of bounds");
+            return true;
+        }
+        // destructure array to improve readability
+        var _b = this.checkField(position), win = _b[0], lose = _b[1];
+        if (win) {
+            // you win
+            console.log("You win by reaching the hat!! congratulations");
+            return true;
+        }
+        if (lose) {
+            // you fell down a hole
+            console.log("You fell down a whole");
+            return true;
+        }
+        // else a legal move
+        // update the Fields currentPosition
+        this.currentPosition = position;
+        return false;
+    };
+    Field.prototype.moveCharacter = function () {
+        this.field[this.currentPosition.first][this.currentPosition.second] = fieldElements[3];
+    };
+    Field.generateRandomFieldItem = function (percentage) {
+        if (percentage === void 0) { percentage = 0.1; }
+        var randomNumber = Math.random();
+        if (randomNumber >= 0 && randomNumber < percentage) {
+            return fieldElements[1];
+        }
+        return fieldElements[2];
+    };
+    Field.placeHatOnField = function (fieldArray, height, width) {
+        var h = 0;
+        var w = 0;
+        do {
+            h = Math.floor(Math.random() * height);
+            w = Math.floor(Math.random() * width);
+        } while (!h || !w);
+        fieldArray[h][w] = fieldElements[0];
+    };
+    Field.generateField = function (height, width, percentage) {
+        var actualPercentage = percentage / 100;
+        var fieldArray = [];
+        for (var i = 0; i < height; i++) {
+            var newArray = [];
+            for (var j = 0; j < width; j++) {
+                newArray.push(Field.generateRandomFieldItem(actualPercentage));
+            }
+            fieldArray.push(newArray);
+        }
+        Field.placeHatOnField(fieldArray, height, width);
+        fieldArray[0][0] = fieldElements[3];
+        return fieldArray;
+    };
+    return Field;
+}());
+var fieldArray = Field.generateField(4, 5, 33);
+var field = new Field(fieldArray);
+var result = false;
 do {
-  field.print();
-
-  const input  = prompt("Where would you like to move?");
-  result = field.checkLegalMove(input)
-
-  field.moveCharacter()
-
-} while( !result)
+    field.print();
+    var input = prompt("Where would you like to move?");
+    result = field.checkLegalMove(input);
+    field.moveCharacter();
+} while (!result);
---
+++ nodeGame.ts
@@ -0,0 +1,128 @@
+import psp from "prompt-sync-plus";
+const prompt = psp(undefined);
+
+const fieldElements = ["^", "O", "░", "*"] as const
+
+type Elements = "^" | "O" | "░" | "*"
+class Field {
+  field: Elements[][];
+  currentPosition: { first: number, second: number };
+  constructor(array2d: Elements[][]) {
+    this.field = array2d;
+    this.currentPosition = {
+      first: 0,
+      second: 0
+    }
+  }
+  print() {
+    for (const row of this.field) {
+      process.stdout.write(row.join(""))
+    }
+  }
+  checkField(position: { first: number, second: number }) {
+    return fieldElements.map(field => this.field[position.first][position.second] === field)
+  }
+  getNewPosition(first: number, second: number) {
+    return {
+      first: first,
+      second: second
+    }
+  }
+  isNewPositionInBoard(position: { first: number, second: number }) {
+    return this.field.length > position.first && position.first >= 0 && this.field[0].length > position.second && position.second >= 0
+  }
+  checkLegalMove(input: string) {
+    const { first, second } = this.currentPosition
+    let position: { first: number, second: number };
+    switch (input) {
+      case 'd':
+        position = this.getNewPosition(first + 1, second)
+        break;
+      case 'l':
+        position = this.getNewPosition(first, second - 1)
+        break;
+      case 'r':
+        position = this.getNewPosition(first, second + 1)
+        break
+      case 'u':
+        position = this.getNewPosition(first - 1, second)
+        break;
+      default:
+        console.log('that is not a correct move\nCorrect moves are l, d, u or r')
+        return true
+    }
+
+    // needed in case movement is legal
+    // result is not in array
+    if (!this.isNewPositionInBoard(position)) {
+      console.log("Area out of bounds");
+      return true
+    }
+    // destructure array to improve readability
+    const [win, lose] = this.checkField(position)
+    if (win) {
+      // you win
+      console.log("You win by reaching the hat!! congratulations")
+      return true
+    }
+    if (lose) {
+      // you fell down a hole
+      console.log("You fell down a whole")
+      return true
+    }
+    // else a legal move
+    // update the Fields currentPosition
+    this.currentPosition = position
+    return false
+  }
+  moveCharacter() {
+    this.field[this.currentPosition.first][this.currentPosition.second] = fieldElements[3]
+  }
+  static generateRandomFieldItem(percentage = 0.1): typeof fieldElements[number] {
+
+    const randomNumber = Math.random()
+    if (randomNumber >= 0 && randomNumber < percentage) {
+      return fieldElements[1];
+    }
+    return fieldElements[2]
+
+  }
+  static placeHatOnField(fieldArray: Elements[][], height: number, width: number) {
+    let h = 0
+    let w = 0
+    do {
+      h = Math.floor(Math.random() * height)
+      w = Math.floor(Math.random() * width)
+    } while (!h || !w)
+
+    fieldArray[h][w] = fieldElements[0]
+  }
+  static generateField(height: number, width: number, percentage: number) {
+    const actualPercentage = percentage / 100
+    const fieldArray: Elements[][] = []
+    for (let i = 0; i < height; i++) {
+      const newArray: Elements[] = []
+      for (let j = 0; j < width; j++) {
+        newArray.push(Field.generateRandomFieldItem(actualPercentage))
+      }
+      fieldArray.push(newArray)
+    }
+    Field.placeHatOnField(fieldArray, height, width)
+    fieldArray[0][0] = fieldElements[3]
+    return fieldArray;
+  }
+}
+
+const fieldArray: Elements[][] = Field.generateField(4, 5, 33)
+
+const field = new Field(fieldArray)
+let result = false
+do {
+  field.print();
+
+  const input = prompt("Where would you like to move?") as string;
+  result = field.checkLegalMove(input)
+
+  field.moveCharacter()
+
+} while (!result)
